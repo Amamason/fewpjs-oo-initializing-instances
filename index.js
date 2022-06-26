af80fdb16911061d655b2accba8ccd4b33addb25
Amamason <104336445+Amamason@users.noreply.github.com>
2022-06-27 09:54:41 +1000

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your code here
+class Breakfast {
+    constructor(food, drink ){
+        this.food = food;
+        this.drink = drink;
+    }
+}
+
+class Lunch {
+    constructor(salad, soup, drink){
+        this.salad = salad;
+        this.soup = soup;
+        this.drink = drink;
+
+    }
+}
+let todaysLunch = new Lunch('ham', 'toast','tea');
+todaysLunch;
+console.log(todaysLunch);
+
+class Dinner {
+    constructor(salad, soup, entree, _dessert){
+        this.salad = salad;
+        this.soup = soup;
+        this.entree = entree;
+        this._dessert = _dessert;
+    }
+}
+
+let tonightsDinner = new Dinner('french', 'peaham', 'prawns', 'chocolate');
+console.log(tonightsDinner);
+
